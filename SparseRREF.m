85fb0ccc0974f84c72afd73b164aed7eb72b0fb8
Li-Xiang-Ideal <54926635+Li-Xiang-Ideal@users.noreply.github.com>
2025-10-01 13:26:04 +0800

    update mma package (deprecated)
    SparseRREF.m updated here is now obsolete; keeping commit for historical clarity.

--- SparseRREF.m
+++ SparseRREF.m
@@ -3,10 +3,10 @@
 $SpaseRREFDirectory=DirectoryName[$InputFileName];
 
 
-Print["Sparse Reduced Row Echelon Form v0.3.3"]
+Print["Sparse Reduced Row Echelon Form v0.3.4"]
 
 
 modrreflib=LibraryFunctionLoad[$SpaseRREFDirectory<>"mathlink.dll","modrref",{{LibraryDataType[SparseArray],"Constant"},{Integer},{Integer},{Integer}},{LibraryDataType[SparseArray],Automatic}];
 ratrreflib=LibraryFunctionLoad[$SpaseRREFDirectory<>"mathlink.dll","rational_rref",{{LibraryDataType[ByteArray],"Constant"},{Integer},{Integer}},{LibraryDataType[ByteArray],Automatic}];
-modprref[mat_SparseArray,p_Integer,method_:1,nthread_:1]:=With[{joinedmat=modrreflib[mat,p,method,nthread]},If[method=!=0,{joinedmat[[;;Length@mat]],joinedmat[[Length@mat+1;;]]},joinedmat]];
+modprref[mat_SparseArray,p_Integer,method_:1,nthread_:1]:=With[{joinedmat=modrreflib[mat,p,method,nthread]},If[method=!=0,{joinedmat[[;;Length@mat]],Transpose[joinedmat[[Length@mat+1;;]]]},joinedmat]];
 ratrref[mat_SparseArray,mode_:1,nthread_:1]:=BinaryDeserialize[ratrreflib[BinarySerialize[mat],mode,nthread]];
