d5de7148e7930726d876c2361201b4d1e9a172db
Li-Xiang-Ideal <54926635+Li-Xiang-Ideal@users.noreply.github.com>
2026-01-03 23:59:10 +0800

    add Dockerfile and update mma package

--- SparseRREF.m
+++ SparseRREF.m
@@ -1,12 +1,13 @@
 (* ::Package:: *)
 
 $SpaseRREFDirectory=DirectoryName[$InputFileName];
+$SpaseRREFLibrary=With[{ext=Switch[$OperatingSystem,"Windows","dll","MacOSX","dylib","Unix","so"]},SortBy[FileNames[$SpaseRREFDirectory<>"mathlink.*"],If[StringExtract[#,"."->-1]===ext,0,1]&][[1]]]
 
 
-Print["Sparse Reduced Row Echelon Form v0.3.4"]
+Print["Sparse Reduced Row Echelon Form v0.3.5"]
 
 
-modrreflib=LibraryFunctionLoad[$SpaseRREFDirectory<>"mathlink.dll","modrref",{{LibraryDataType[SparseArray],"Constant"},{Integer},{Integer},{Integer}},{LibraryDataType[SparseArray],Automatic}];
-ratrreflib=LibraryFunctionLoad[$SpaseRREFDirectory<>"mathlink.dll","rational_rref",{{LibraryDataType[ByteArray],"Constant"},{Integer},{Integer}},{LibraryDataType[ByteArray],Automatic}];
+modrreflib=LibraryFunctionLoad[$SpaseRREFLibrary,"modrref",{{LibraryDataType[SparseArray],"Constant"},{Integer},{Integer},{Integer}},{LibraryDataType[SparseArray],Automatic}];
+ratrreflib=LibraryFunctionLoad[$SpaseRREFLibrary,"rational_rref",{{LibraryDataType[ByteArray],"Constant"},{Integer},{Integer}},{LibraryDataType[ByteArray],Automatic}];
 modprref[mat_SparseArray,p_Integer,method_:1,nthread_:1]:=With[{joinedmat=modrreflib[mat,p,method,nthread]},If[method=!=0,{joinedmat[[;;Length@mat]],Transpose[joinedmat[[Length@mat+1;;]]]},joinedmat]];
 ratrref[mat_SparseArray,mode_:1,nthread_:1]:=BinaryDeserialize[ratrreflib[BinarySerialize[mat],mode,nthread]];
